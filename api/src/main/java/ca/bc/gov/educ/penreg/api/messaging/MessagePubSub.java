10f2b57cf84260c467d23a61d1cc62adf0c3c7fc
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2020-11-19 11:53:15 -0800

    removing unnecessary thread.

--- api/src/main/java/ca/bc/gov/educ/penreg/api/messaging/MessagePubSub.java
+++ api/src/main/java/ca/bc/gov/educ/penreg/api/messaging/MessagePubSub.java
@@ -1,13 +1,10 @@
 package ca.bc.gov.educ.penreg.api.messaging;
 
 import io.nats.client.Connection;
-import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.Closeable;
 import java.util.Optional;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 /**
  * The type Message pub sub.
@@ -19,20 +16,12 @@ public abstract class MessagePubSub implements Closeable {
    * The Connection.
    */
   protected Connection connection;
-  /**
-   * The Executor service.
-   */
-  @Setter
-  protected ExecutorService executorService = Executors.newSingleThreadExecutor();
 
   /**
    * Close.
    */
   @Override
   public void close() {
-    if (!executorService.isShutdown()) {
-      executorService.shutdown();
-    }
     if(Optional.ofNullable(connection).isPresent()){
       log.info("closing nats connection...");
       try {
