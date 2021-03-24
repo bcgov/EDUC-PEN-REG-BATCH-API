0db10f6d9c71e7e7324c81aed7ba86d383acc87c
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2021-03-24 12:46:38 -0700

    fix broken tests...

--- api/src/test/java/ca/bc/gov/educ/penreg/api/batch/service/PenRequestBatchFileServiceTest.java
+++ api/src/test/java/ca/bc/gov/educ/penreg/api/batch/service/PenRequestBatchFileServiceTest.java
@@ -13,6 +13,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -28,6 +29,7 @@ import static org.mockito.Mockito.when;
 @RunWith(SpringRunner.class)
 @ActiveProfiles("test")
 @SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class PenRequestBatchFileServiceTest {
   @Autowired
   RestUtils restUtils;
