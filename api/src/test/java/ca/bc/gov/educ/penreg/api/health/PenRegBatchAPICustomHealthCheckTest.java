8af253fbe84e3116748c5db867156b721ab37b5a
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2021-10-12 09:25:26 -0700

    [FIX] Add Redis to health check

--- api/src/test/java/ca/bc/gov/educ/penreg/api/health/PenRegBatchAPICustomHealthCheckTest.java
+++ api/src/test/java/ca/bc/gov/educ/penreg/api/health/PenRegBatchAPICustomHealthCheckTest.java
@@ -2,9 +2,21 @@ package ca.bc.gov.educ.penreg.api.health;
 
 import ca.bc.gov.educ.penreg.api.BasePenRegAPITest;
 import io.nats.client.Connection;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
+import org.redisson.api.RedissonClient;
+import org.redisson.api.redisnode.RedisCluster;
+import org.redisson.api.redisnode.RedisClusterMaster;
+import org.redisson.api.redisnode.RedisClusterSlave;
+import org.redisson.api.redisnode.RedisNodes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.health.Status;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.concurrent.TimeUnit;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
@@ -13,13 +25,20 @@ public class PenRegBatchAPICustomHealthCheckTest extends BasePenRegAPITest {
 
   @Autowired
   Connection natsConnection;
+  @MockBean
+  RedissonClient redissonClient;
 
+  @Before
+  public void setup() {
+    Mockito.reset(redissonClient);
+  }
   @Autowired
   private PenRegBatchAPICustomHealthCheck penRegBatchAPICustomHealthCheck;
 
   @Test
   public void testGetHealth_givenClosedNatsConnection_shouldReturnStatusDown() {
     when(this.natsConnection.getStatus()).thenReturn(Connection.Status.CLOSED);
+    when(redissonClient.getRedisNodes(RedisNodes.CLUSTER)).thenReturn(null);
     assertThat(this.penRegBatchAPICustomHealthCheck.getHealth(true)).isNotNull();
     assertThat(this.penRegBatchAPICustomHealthCheck.getHealth(true).getStatus()).isEqualTo(Status.DOWN);
   }
@@ -27,6 +46,7 @@ public class PenRegBatchAPICustomHealthCheckTest extends BasePenRegAPITest {
   @Test
   public void testGetHealth_givenOpenNatsConnection_shouldReturnStatusUp() {
     when(this.natsConnection.getStatus()).thenReturn(Connection.Status.CONNECTED);
+    when(redissonClient.getRedisNodes(RedisNodes.CLUSTER)).thenReturn(this.getRedisClusterMock(true));
     assertThat(this.penRegBatchAPICustomHealthCheck.getHealth(true)).isNotNull();
     assertThat(this.penRegBatchAPICustomHealthCheck.getHealth(true).getStatus()).isEqualTo(Status.UP);
   }
@@ -35,6 +55,7 @@ public class PenRegBatchAPICustomHealthCheckTest extends BasePenRegAPITest {
   @Test
   public void testHealth_givenClosedNatsConnection_shouldReturnStatusDown() {
     when(this.natsConnection.getStatus()).thenReturn(Connection.Status.CLOSED);
+    when(redissonClient.getRedisNodes(RedisNodes.CLUSTER)).thenReturn(this.getRedisClusterMock(false));
     assertThat(this.penRegBatchAPICustomHealthCheck.health()).isNotNull();
     assertThat(this.penRegBatchAPICustomHealthCheck.health().getStatus()).isEqualTo(Status.DOWN);
   }
@@ -42,7 +63,42 @@ public class PenRegBatchAPICustomHealthCheckTest extends BasePenRegAPITest {
   @Test
   public void testHealth_givenOpenNatsConnection_shouldReturnStatusUp() {
     when(this.natsConnection.getStatus()).thenReturn(Connection.Status.CONNECTED);
+    when(redissonClient.getRedisNodes(RedisNodes.CLUSTER)).thenReturn(this.getRedisClusterMock(true));
     assertThat(this.penRegBatchAPICustomHealthCheck.health()).isNotNull();
     assertThat(this.penRegBatchAPICustomHealthCheck.health().getStatus()).isEqualTo(Status.UP);
+  }
+
+  private RedisCluster getRedisClusterMock(boolean pingAll) {
+    return new RedisCluster() {
+      @Override
+      public Collection<RedisClusterMaster> getMasters() {
+        return Collections.emptyList();
+      }
+
+      @Override
+      public RedisClusterMaster getMaster(String address) {
+        return null;
+      }
+
+      @Override
+      public Collection<RedisClusterSlave> getSlaves() {
+        return null;
+      }
+
+      @Override
+      public RedisClusterSlave getSlave(String address) {
+        return null;
+      }
+
+      @Override
+      public boolean pingAll() {
+        return pingAll;
+      }
+
+      @Override
+      public boolean pingAll(long timeout, TimeUnit timeUnit) {
+        return false;
+      }
+    };
   }
 }
