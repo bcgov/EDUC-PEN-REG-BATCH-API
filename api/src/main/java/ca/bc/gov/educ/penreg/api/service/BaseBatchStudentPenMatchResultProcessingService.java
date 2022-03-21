5d562b71110daed7f08dd861d6f0a21fb9d62cb1
arcshiftsolutions <arcshiftsolutions@gmail.com>
2022-03-21 16:04:17 -0700

    Comments updated.

--- api/src/main/java/ca/bc/gov/educ/penreg/api/service/BaseBatchStudentPenMatchResultProcessingService.java
+++ api/src/main/java/ca/bc/gov/educ/penreg/api/service/BaseBatchStudentPenMatchResultProcessingService.java
@@ -292,8 +292,7 @@ public abstract class BaseBatchStudentPenMatchResultProcessingService extends Ba
   /**
    * updated for https://gww.jira.educ.gov.bc.ca/browse/PEN-1348
    * When district number is <b> NOT </b> 102, apply the following logic for grade code & grade year.
-   * If PEN Request grade code is null, and STUDENT record grade code is null do nothing
-   * If PEN Request grade code has value, set it in the STUDENT record
+   * If the student record grade is null, and the incoming batch grade is valid, take it and update the grade year
    * Set the STUD_GRADE_YEAR to the current year (if after June 30) or the previous year (if before June 30)
    *
    * @param studentFromStudentAPI          the student from student api
